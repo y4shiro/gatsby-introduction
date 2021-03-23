a0b08277f227e879274bf21a8770222d869df571
y4shiro <y4shironao@gmail.com>
2021-03-23 22:01:26 +0900

    13. フッターの作成

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../styles/footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">© {new Date().getFullYear()} Dev Blog</footer>
+  )
+}
+
+export default Footer
