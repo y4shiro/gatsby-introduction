e2035f979beead17906dec03b6911b2e4600a0cc
y4shiro <y4shironao@gmail.com>
2021-03-23 22:12:59 +0900

    15. GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "Dev Blog",
+    description: "Gatsby で作成したブログサイトです",
+    author: "y4shiro",
+  },
   plugins: [],
 }
