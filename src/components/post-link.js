13bc2cfa1955344dedb8a1fb86ca868359d89446
y4shiro <y4shironao@gmail.com>
2021-03-24 21:10:11 +0900

    18. 記事データの取得

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,18 +1,23 @@
 import React from "react"
 import "../styles/post-link.css"
 
-const RANDOM_IMG = "https://source.unsplash.com/collection/175083/640x360"
+const PostLink = ({ post }) => {
+  const { title, updatedAt, image } = post
+  const description = post.description.description
 
-const PostLink = () => {
   return (
     <div className="post-link">
       <div>
-        <img src={RANDOM_IMG} className="post-link-image" alt="post-cover" />
+        <img
+          src={image.file.url}
+          className="post-link-image"
+          alt="post-cover"
+        />
       </div>
       <div className="post-link-text">
-        <h2>Gatsby でブログを作成してみた</h2>
-        <p className="post-link-body">Gatsby 初めた</p>
-        <p className="post-link-date">2020/05/30</p>
+        <h2>{title}</h2>
+        <p className="post-link-body">{description}</p>
+        <p className="post-link-date">{updatedAt}</p>
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,38 @@ import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import PostLink from "../components/post-link"
+import { graphql } from "gatsby"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
       <Hero />
-      <PostLink />
-      <PostLink />
-      <PostLink />
+      {data.allContentfulPost.edges.map(edge => (
+        <PostLink key={edge.node.slug} post={edge.node} />
+      ))}
     </Layout>
   )
 }
+
+export const query = graphql`
+  query allContentfulPost {
+    allContentfulPost {
+      edges {
+        node {
+          title
+          image {
+            title
+            file {
+              url
+            }
+          }
+          description {
+            description
+          }
+          slug
+          updatedAt(locale: "ja-JP", formatString: "YYYY年MM月DD日")
+        }
+      }
+    }
+  }
+`
